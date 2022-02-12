ca3e928dbfe0dc72a81a1baffe3ff1d73fced853
TheBlueChameleon <stefan_hartinger@gmx.de>
2022-02-12 16:57:32 +0100

    first commit

---
+++ src/main-unittest.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Unittest" << endl;
+    return 0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Main Project" << endl;
+    return 0;
+}
