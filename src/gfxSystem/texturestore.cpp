f03a6b21bf30944e59ef9580b8348437f105ea80
TheBlueChameleon <stefan_hartinger@gmx.de>
2022-02-13 17:12:32 +0100

    add TextureStore stub

--- src/gfxSystem/texturestore.cpp
+++ src/gfxSystem/texturestore.cpp
@@ -1,6 +1,148 @@
+// ========================================================================== //
+// Depenencies
+
+// STL
+#include <string>
+using namespace std::string_literals;
+
+#include <filesystem>
+namespace fs = std::filesystem;
+
+// local
 #include "texturestore.hpp"
 
-TextureStore::TextureStore()
-{
+// ========================================================================== //
+// local macro
+
+#define THROWTEXT(msg) ("RUNTIME EXCEPTION IN "s + (__PRETTY_FUNCTION__) + "\n"s + msg)
+
+#ifdef DEBUG
+#define CHECK_GFX_INDEX(ID) { \
+        if ( (ID >= this->size()) ) { \
+            throw std::out_of_range(THROWTEXT( "  Invalid GfxStore ID: "s + std::to_string(ID) )); \
+        } \
+    }
+#else
+#define CHECK_GFX_INDEX(ID) {}
+#endif
+
+#define CHECK_FILE_EXISTS(filename) {\
+        if (!fs::exists(filename)) { \
+            throw std::runtime_error(THROWTEXT( \
+                                                "  file not found: '"s + filename + "'" \
+                                              )); \
+        } \
+    }
+
+// ========================================================================== //
+// namespace
+
+namespace RetrogameBase {
+
+// ========================================================================== //
+// CTor, DTor
+
+    TextureStore::TextureStore(const Window* win) :
+        win(win) {}
+
+    TextureStore::~TextureStore() {
+        reset();
+    }
+
+// ========================================================================== //
+// getters
+
+    const Window& TextureStore::getWin() const {
+        return *win;
+    }
+
+    size_t TextureStore::size() const {
+        return textures.size();
+    }
+
+    SDL_Texture* TextureStore::getTexture(size_t ID) const {
+        CHECK_GFX_INDEX(ID);
+        return textures[ID];
+    }
+
+    const std::string& TextureStore::getFilename(const int ID) const {
+        CHECK_GFX_INDEX(ID);
+        return filenames[ID];
+    }
+
+    std::pair<int, int> TextureStore::getImageDimensions(const int ID) const {
+        CHECK_GFX_INDEX(ID);
+        return dimensions[ID];
+    }
+
+    size_t TextureStore::findByFilename(const std::string& filename) const {
+        auto searchIterator = std::find(filenames.begin(),
+                                        filenames.end(),
+                                        filename);
+        if (searchIterator != filenames.end()) {
+            return std::distance(filenames.begin(), searchIterator);
+        } else {
+            return NOINDEX;
+        }
+    }
+
+// ========================================================================== //
+// setters/modifiers
+
+    void TextureStore::reset() {
+        for (auto& tex : textures) {
+            SDL_DestroyTexture(tex);
+        }
+    }
+
+    size_t TextureStore::addFrame(const std::string& filename) {
+        CHECK_FILE_EXISTS(filename);
+
+        auto index = findByFilename(filename);
+
+        if (index != NOINDEX) {
+            return index;
+        }
+
+        SDL_Surface* loadedSurface = IMG_Load( filename.c_str() );
+
+        if( loadedSurface == NULL ) {
+            throw std::runtime_error(THROWTEXT(
+                                         "Unable to load image " + filename + "!\n"
+                                         "\tSDL_image Error: " + IMG_GetError()
+                                     ));
+        }
+
+        SDL_Texture* newTexture = SDL_CreateTextureFromSurface(win->getRenderer(),
+                                  loadedSurface);
+        if(!newTexture) {
+            throw std::runtime_error(THROWTEXT(
+                                         "Unable to create texture from image " + filename + "!\n"
+                                         "\tSDL_image Error: " + IMG_GetError()
+                                     ));
+        }
+
+        filenames.push_back(filename);
+        textures.push_back(newTexture);
+        dimensions.push_back( std::make_pair(loadedSurface->w, loadedSurface->h) );
+
+        SDL_FreeSurface( loadedSurface );
+
+        return textures.size() - 1;
+    }
+
+// ========================================================================== //
+// display
+
+    void TextureStore::put(int ID, int x, int y) {
+        CHECK_GFX_INDEX(ID);
+
+        SDL_Rect dest = {x, y, dimensions[ID].first, dimensions[ID].second};
+
+        SDL_RenderCopy(win->getRenderer(), textures[ID], NULL, &dest);
+    }
+
+// ========================================================================== //
+// namespace
 
 }
--- src/gfxSystem/texturestore.hpp
+++ src/gfxSystem/texturestore.hpp
@@ -1,11 +1,68 @@
 #ifndef TEXTURESTORE_HPP
 #define TEXTURESTORE_HPP
 
+// ========================================================================== //
+// Depenencies
 
-class TextureStore
-{
-public:
-    TextureStore();
-};
+// STL
+#include <stdexcept>
 
+#include <vector>
+#include <string>
+
+// SDL
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+// own
+#include "window.hpp"
+
+// ========================================================================== //
+// Class
+
+namespace RetrogameBase {
+    class TextureStore {
+        private:
+            const Window* win;
+
+            std::vector<SDL_Texture*>        textures;
+            std::vector<std::string>         filenames;
+            std::vector<std::pair<int, int>> dimensions;
+
+        public:
+            static const size_t NOINDEX = std::numeric_limits<size_t>::max();
+
+            // -------------------------------------------------------------- //
+            // CTor, DTor
+            TextureStore() = delete;
+            TextureStore(const Window* win);
+            ~TextureStore();
+
+            // -------------------------------------------------------------- //
+            // getters
+
+            const Window& getWin () const;
+
+            size_t size() const;
+
+            SDL_Texture*        getTexture(size_t ID) const;
+            const std::string&  getFilename(const int ID) const;
+            std::pair<int, int> getImageDimensions(const int ID) const;
+
+            size_t findByFilename(const std::string& filename) const;         // returns NOINDEX if image not in store
+
+            // -------------------------------------------------------------- //
+            // setters/modifiers
+
+            void reset();
+
+            size_t addFrame (const std::string& filename);                    // returns index of loaded image in store. Prevents double loading
+
+            // -------------------------------------------------------------- //
+            // display
+
+            void put (int ID, int x, int y);
+    };
+
+}
 #endif // TEXTURESTORE_HPP
